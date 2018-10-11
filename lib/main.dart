9530c1d14a2906ef76ae10a5beec0f7f3d74c898
Jonas <jwallat@hotmail.de>
2018-10-11 18:18:54 +0200

    outsourced the widget construction to external file

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,8 @@
 // I need to import a helper library from flutter to get content on screen
 import "package:flutter/material.dart";
+import "src/app.dart";
 
-// Define a main 
+// Define a main
 void main() {
-// Create text widget
-var app = MaterialApp(
-  home: Scaffold(
-    appBar: AppBar(
-      title: Text("Let's see some images")
-      )
-    )
-);
-
-runApp(app);
-// Take widget and get it on screen
+  runApp(App());
 }
---
+++ lib/src/app.dart
@@ -0,0 +1,21 @@
+// Import flutter helper library
+import "package:flutter/material.dart";
+
+// Create class for custom widget this class must extend the statelessWidget base class
+class App extends StatelessWidget {
+// Must define "build" method
+  Widget build(BuildContext buildContext) {
+    return MaterialApp(
+      home: Scaffold(
+        floatingActionButton: FloatingActionButton(
+            onPressed: () {
+              print("Please don't");
+            },
+            child: Icon(Icons.add)),
+        appBar: AppBar(
+          title: Text("Let's see some images"),
+        ),
+      ),
+    );
+  }
+}
