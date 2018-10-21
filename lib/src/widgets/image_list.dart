c28a07c503eb996122188aa14d7ea34f2571ad92
Jonas <jwallat@hotmail.de>
2018-10-21 15:46:24 +0200

    finalized pictures app with margin, padding

--- lib/src/widgets/image_list.dart
+++ lib/src/widgets/image_list.dart
@@ -10,8 +10,32 @@ class ImageList extends StatelessWidget {
     return ListView.builder(
       itemCount: images.length,
       itemBuilder: (context, int index) {
-        return Text(images[index].url);
+        return buildImage(images[index]);
       },
+    );
+  }
+
+  Widget buildImage(ImageModel image) {
+    return Container(
+      margin: EdgeInsets.all(20.0),
+      padding: EdgeInsets.all(20.0),
+      decoration: BoxDecoration(
+        border: Border.all(
+          color: Colors.grey,
+          width: 2.0,
+        ),
+      ),
+      child: Column(
+        children: <Widget>[
+          Padding(
+            child: Image.network(image.url),
+            padding: EdgeInsets.only(
+              bottom: 8.0,
+            ),
+          ),
+          Text(image.title),
+        ],
+      ),
     );
   }
 }
